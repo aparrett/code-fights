6fa2d6875269e222270274d78bcf2a1a4e8b777d
Anthony Parrett <anthonyparrett7@gmail.com>
2018-08-08 21:43:30 -0500

    Is Digit

--- js/elections-winners.js
+++ js/elections-winners.js
@@ -1,19 +1,19 @@
 function electionsWinners(votes, k) {
-    let possibleWinners = 0;
-    const max = Math.max(...votes);
+  let possibleWinners = 0;
+  const max = Math.max(...votes);
+  
+  if (k === 0) {
+    const candidateOne = votes.indexOf(max);
+    const candidateTwo = votes.lastIndexOf(max);
     
-    if (k === 0) {
-      const candidateOne = votes.indexOf(max);
-      const candidateTwo = votes.lastIndexOf(max);
-      
-      return candidateOne === candidateTwo ? 1 : 0;
-    }
-    
-    for (let i = 0; i < votes.length; i++) {
-      if (votes[i] + k > max) {
-        possibleWinners++;
-      } 
-    }
-    
-    return possibleWinners;
+    return candidateOne === candidateTwo ? 1 : 0;
   }
+  
+  for (let i = 0; i < votes.length; i++) {
+    if (votes[i] + k > max) {
+      possibleWinners++;
+    } 
+  }
+  
+  return possibleWinners;
+}
